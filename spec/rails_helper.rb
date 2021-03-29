04d380dfd03ff3357ca517f3bc60f2df1b7ed27e
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-30 00:14:10 +0900

    Fakerの環境構築

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+# 日本語のダミーデータを使用
+Faker::Config.locale = :ja
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
